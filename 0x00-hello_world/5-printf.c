116d9f35025ed2ee710c5cfcef55c4290d36e310
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-08 19:15:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
