678ee993a673e4d196239731976dc3ed45d93dab
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-10 07:07:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	const char part1[] = "and that piece of art is useful\"";
-	const char part2[] = " - Dora Korpar, 2015-10-19\n";
+	char part1[] = "and that piece of art is useful\"";
+	char part2[] = " - Dora Korpar, 2015-10-19\n";
 
 	syscall(SYS_write, 1, part1, sizeof(part1));
 	syscall(SYS_write, 1, part2, sizeof(part2));
