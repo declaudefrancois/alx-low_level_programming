e14c02331079b291b8cd94617208aae955fec1a9
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-09 17:12:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
