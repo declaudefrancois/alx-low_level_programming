626a39e6878538e02a59e50f0437d108c76d2b9f
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-14 05:52:43 -0700

    alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while(a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+
+	return (0);
+}
+
