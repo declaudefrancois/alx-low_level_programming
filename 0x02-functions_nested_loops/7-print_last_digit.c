ea7188cc96fbd3a288e9e07dd172a3a3e04f230e
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-14 13:47:13 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -9,6 +10,12 @@
 int print_last_digit(int n)
 {
 	int last_digit = n < 0 ? -n % 10 : n % 10;
+
+	if (n == INT_MIN)
+	{
+		_putchar('8');
+		return (8);
+	}
 
 	_putchar(last_digit + '0');
 	return (last_digit);
