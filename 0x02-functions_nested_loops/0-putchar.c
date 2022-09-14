93f0c3276f3910702f4d8f06c71814f1a6164f8b
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-14 05:05:25 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+
+	return (0);
+}
+
