88e4376dd11b13ce4e151b9ab6d53673d4b93655
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-14 18:17:14 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int n = 0, sum = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+
+		n++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
