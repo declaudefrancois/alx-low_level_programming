c8200cb196dd424039e9288e9999ebf4e11c7ef3
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-15 16:45:17 -0700

    avoid 8 loop

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	if (positive_or_negative(0) == 0)
+		printf("0 is zero");
+	else
+		printf("0 is not zero");
+
+        return (0);
+}
