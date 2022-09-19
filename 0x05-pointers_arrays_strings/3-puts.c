1556cc84ccd03870bfd639339feabd5872efa9bd
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-19 07:06:10 -0700

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by new line.
+ * @str: the string to print
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
