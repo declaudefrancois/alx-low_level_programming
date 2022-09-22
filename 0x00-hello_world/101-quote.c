4eb523d80300dec005314a9d866fdeae01c44ff9
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-21 17:06:34 -0700

    write syscall

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,8 +11,8 @@ int main(void)
 	char part1[] = "and that piece of art is useful\"";
 	char part2[] = " - Dora Korpar, 2015-10-19\n";
 
-	write(1, part1, 33);
-	write(1, part2, 29);
+	write(1, part1, 31);
+	write(1, part2, 28);
 	return (1);
 }
 
