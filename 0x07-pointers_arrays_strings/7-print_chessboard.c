5ac61f186fea84a77bd6b03e0cbb0cbdab04cb87
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-27 15:54:35 -0700

    print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chessboard.
+ * @a: a 8x8 two dimensional array.
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
