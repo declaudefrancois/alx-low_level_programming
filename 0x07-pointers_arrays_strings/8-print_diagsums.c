c68e27c8ef95abdea866e3fd55d9a2fe2ee41136
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-27 18:15:36 -0700

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,10 +13,10 @@ void print_diagsums(int *a, int size)
 	int i, s1 = 0, s2 = 0;
 
 	for (i = 0; i < size; i++)
+	{
 		s1 += *(a + i * (size + 1));
-
-	for (i = size - 1; i >= 0; i--)
-		s2 += *(a + i * (size + 1));
+		s2 += *(a + (i * size) + (size - i - 1));
+	}
 
 	printf("%d, %d\n", s1, s2);
 }
