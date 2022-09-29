1b33dbd3dd79e574ac38d491ab680f1e1babded3
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-09-29 13:32:32 -0700

    print rev recusion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * using recursion.
+ * @s: a pointer to the string to print.
+ *
+ * Return: Void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
