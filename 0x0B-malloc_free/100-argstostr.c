487b5c062a2be01e2bbaaf86c5cb0749a6abc031
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-04 18:06:25 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+int _strlen(char *s);
 
 /**
  * argstostr - concatenates all the argument received by main.
@@ -9,4 +10,53 @@
  */
 char *argstostr(int ac, char **av)
 {
+	char *s;
+	int size = 0, i, j, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		size += _strlen(*(av + i));
+
+	s = (char *) malloc(sizeof(char) * (size + 1));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			s[k] = av[i][j];
+			k++;
+			j++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+
+	return (s);
+}
+
+/**
+ * _strlen - string length
+ * @s: the string to get the length
+ *
+ * Return: int the string's length
+ */
+int _strlen(char *s)
+{
+	int i = 0, size = 0;
+
+	while (s[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+
+	return (size);
 }
