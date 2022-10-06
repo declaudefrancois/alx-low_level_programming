04b194af4b2bee5999d7554d4f4ee941fa39ae68
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-06 15:21:04 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,6 +14,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,6 +22,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		*(((char *)p) + i) = 0;
 
 	return (p);
 }
