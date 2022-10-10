3163c2283ccf33c59b55862fc57f3783a2e07561
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-10 15:47:48 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ *
+ * Description: This structure represents a dog.
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif /* DOG_H  */
