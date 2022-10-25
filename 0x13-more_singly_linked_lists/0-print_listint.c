d9aab3865d1f8beebc63e943f7308869f5a93fa3
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-10-25 15:05:16 -0700

    feat: function that print all elements of a singly linked list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *current;
+	int length = 0;
+
+	if (h == NULL)
+		return (0);
+
+	length++;
+	printf("%d\n", h->n);
+	current = h->next; 
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		length++;
+	}
+
+	return (length);
+}
