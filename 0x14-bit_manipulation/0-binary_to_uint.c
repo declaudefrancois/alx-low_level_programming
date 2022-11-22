5c72a5dc8fbc1e3b932fa58f9b2c4de93c40179a
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-11-22 00:50:40 -0800

    feat: convert a binary number to an unsigned decimal int , using bits operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,40 +10,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0,
-		     power = 0,
-		     j = 0,
-		     power2;
-	int len = 0;
+	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len] != '\0')
+	while (*b)
 	{
-		if (b[len] != '1' && b[len] != '0')
+		if (*b != '0' && *b != '1')
 			return (0);
-		len++;
-	}
-	--len;
 
-	while (len >= 0 && b[len])
-	{
-		if (b[len] == '1')
-		{
-			if (power == 0)
-				num++;
-			else
-			{
-				power2 = 1;
-				for (j = 1 ; j <= power; j++)
-					power2 *= 2;
-				num += power2;
-			}
-		}
-
-		len--;
-		power++;
+		num <<= 1;
+		if (*b & 1)
+			num += 1;
+		b++;
 	}
 
 	return (num);
