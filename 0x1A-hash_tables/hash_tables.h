24a5f551526b639cc90ee3103cc433ff16843454
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-06 06:57:59 -0800

    feat(0x1A): function that creates a hash table.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +29,14 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
+#endif /*HASH_TABLE_H*/
+
+
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdlib.h>
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /*INCLUDES_H*/
