7c90c402aa4c794a62f2ff9f9cadd7a13dd37069
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-03-16 01:14:17 +0000

    feat(0x13): reverse a singly linked list.

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint -  reverses a listint_t linked list.
+ *
+ * @head: A double pointer to the head of the list.
+ * Returns: a pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *tmp1;
+	
+	if (!head)
+		return (NULL);
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		(*head)->next = tmp1;
+		tmp1 = *head;
+		*head = tmp;
+	}
+
+	*head = tmp1;
+	return (tmp1);
+}
