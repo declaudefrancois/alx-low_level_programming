0063b1b94212cdd04405c6504a3a1ecdc1bcc726
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-04-14 00:34:19 +0000

    feat(0x1E): exponential search algorithm implementation.

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,26 +1,55 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an array of integers
+ * exponential_search - searches for a value in a sorted array of integers
+ *                      using the Exponential search algorithm.
+ *
+ * @array: a pointer to the first element of the array to search in.
+ * @size: number of elements in array.
+ * @value: the value to search for.
+ *
+ * Return: the first index where value is located OR -1 if not found.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t bound, high;
+
+	if (array == NULL)
+		return (-1);
+	bound = 1;
+	while (bound < size && array[bound] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
+		bound *= 2;
+	}
+
+	high = bound > size ? size - 1 : bound - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", bound / 2, high);
+	return (binary_search_bound(array, bound / 2, high, value));
+}
+
+/**
+ * binary_search_bound - searches for a value in an array of integers
  *                 using the Binary search algorithm.
  *
  * @array: a pointer to the first element of the array to search in.
  *         array is sorted in ascending order.
- * @size: the number of elements in array.
+ * @min: the lower bound index from which to start.
+ * @max: the upper bound index at which to stop.
  * @value: the value to search for.
  *
  * Return: the first index where value is located
  *         or -1 if value is not found in array.
  */
-int binary_search(int *array, size_t size, int value)
+int binary_search_bound(int *array, size_t min, size_t max, int value)
 {
 	size_t left_idx, right_idx, middle;
 
 	if (array == NULL)
 		return (-1);
 
-	left_idx = 0;
-	right_idx = size - 1;
+	left_idx = min;
+	right_idx = max - 1;
 	while (left_idx <= right_idx)
 	{
 		print_array(array, left_idx, right_idx);
