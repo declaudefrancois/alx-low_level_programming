f8fff6352fc61edf63efeb50f4e638ab35025efc
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2024-02-08 00:21:36 +0100

    defer to #c22

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# creates a dynamic library called liball.so from all the .c files that are in the current directory.
-gcc -fPIC -shared -o liball.so *.c
--- 0x1A-hash_tables/README.md
+++
@@ -1,1 +0,0 @@
-# Project 0x1A. C - Hash tables
